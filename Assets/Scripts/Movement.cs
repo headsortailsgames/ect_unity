f72895a6bebddbdb51f88ae80a02b21f007d90ed
Luciano Prestes Cavalcanti <lucianopcbr@gmail.com>
2017-10-22 11:27:50 -0200

    Add Movement script

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	public float speed = 1f;
+
+	private new Rigidbody rigidbody;
+
+
+	void Awake () {
+		this.rigidbody = GetComponent<Rigidbody> ();
+	}
+
+	void Update () {
+		Move ();
+	}
+
+	public void Move () {
+		Vector3 velocity = new Vector3 ();
+		velocity.x = Input.GetAxis("Horizontal");
+		velocity.z = Input.GetAxis ("Vertical");
+
+		velocity *= this.speed;
+
+		this.rigidbody.velocity = velocity;
+	}
+}
