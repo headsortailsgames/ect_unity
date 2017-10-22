b1ed265c51ca4e947bc83e245c3c0e39002db1a3
Luciano Prestes Cavalcanti <lucianopcbr@gmail.com>
2017-10-22 14:33:51 -0200

    Stop moving while attacking

--- Assets/Scripts/Attack.cs
+++ Assets/Scripts/Attack.cs
@@ -6,19 +6,23 @@ public class Attack : MonoBehaviour {
 
 	private bool isAttacking = false;
 	private Animator animator;
+	private Movement movement;
 
 	void Awake() {
 		this.animator = this.transform.Find("Model").GetComponent<Animator> ();
+		this.movement = GetComponent<Movement> ();
 	}
 
 	void Update() {
 		if (!this.isAttacking && Input.GetButtonDown ("Attack")) {
 			this.isAttacking = true;
+			this.movement.enabled = false;
 			this.animator.SetTrigger ("Attack");
 		}
 	}
 
 	public void OnAttackEnd() {
 		this.isAttacking = false;
+		this.movement.enabled = true;
 	}
 }
