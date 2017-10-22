9041cceee508690cdfd9c9442a23372cf35c06e9
Luciano Prestes Cavalcanti <lucianopcbr@gmail.com>
2017-10-22 14:29:36 -0200

    Add attack animation and attack end event

---
+++ Assets/Scripts/Attack.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour {
+
+	private bool isAttacking = false;
+	private Animator animator;
+
+	void Awake() {
+		this.animator = this.transform.Find("Model").GetComponent<Animator> ();
+	}
+
+	void Update() {
+		if (!this.isAttacking && Input.GetButtonDown ("Attack")) {
+			this.isAttacking = true;
+			this.animator.SetTrigger ("Attack");
+		}
+	}
+
+	public void OnAttackEnd() {
+		this.isAttacking = false;
+	}
+}
---
+++ Assets/Scripts/PlayerAnimationEvents.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationEvents : MonoBehaviour {
+
+	private Attack attack;
+
+	void Awake() {
+		this.attack = GetComponentInParent<Attack> ();
+	}
+
+	void OnAttackEnd() {
+		this.attack.OnAttackEnd ();
+	}
+}
