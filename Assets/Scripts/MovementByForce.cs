f6b855c5f9fac9778da9c92c4fa9dae1bb06c2dc
Luciano Prestes Cavalcanti <lucianopcbr@gmail.com>
2017-10-22 11:41:10 -0200

    Add MovementByForce script

---
+++ Assets/Scripts/MovementByForce.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementByForce : MonoBehaviour {
+
+	public float multiplier = 10f;
+
+	private new Rigidbody rigidbody;
+
+
+	void Awake () {
+		this.rigidbody = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate () {
+		Move ();
+	}
+
+	public void Move () {
+		Vector3 force = new Vector3 ();
+		force.x = Input.GetAxis("Horizontal");
+		force.z = Input.GetAxis ("Vertical");
+
+		force *= this.multiplier;
+
+		this.rigidbody.AddForce (force);
+	}
+}
