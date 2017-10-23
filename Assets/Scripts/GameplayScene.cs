f477f63819fff2fde5092b7722f7824f6b30422c
Luciano Prestes Cavalcanti <lucianopcbr@gmail.com>
2017-10-23 09:22:00 -0200

    Back to MainMenu after game over

---
+++ Assets/Scripts/GameplayScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameplayScene : MonoBehaviour {
+
+	public float timeToChangeScene = 5f;
+
+
+	public void OnGameover () {
+		Invoke ("LoadMainMenuScene", this.timeToChangeScene);
+	}
+
+	private void LoadMainMenuScene () {
+		SceneManager.LoadScene ("MainMenu");
+	}
+}
